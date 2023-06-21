3260475b47211e28142541933f9a93ba6ed2c6b3
cindyonggirawan <cindy.onggirawan@binus.ac.id>
2023-06-21 17:34:26 +0700

    Add hidden member collision and member collected display

--- 2DGameExploration/Scenes/Level 1/GameScene.swift
+++ 2DGameExploration/Scenes/Level 1/GameScene.swift
@@ -12,6 +12,9 @@ class GameScene: SKScene {
     let disk = SKSpriteNode(imageNamed: "disk")
     let knob = SKSpriteNode(imageNamed: "knob")
     
+    let membersCollectedLabel = SKLabelNode()
+    var numberOfMembersCollected: Int = 0
+    
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
     
@@ -27,8 +30,8 @@ class GameScene: SKScene {
     
     var isPressed: Bool = false
     
-//    var player = SKSpriteNode(imageNamed: "up")
-    var player = SKSpriteNode(imageNamed: "character")
+    var player: SKSpriteNode!
+    var hiddenMembers: [SKSpriteNode] = []
     
     override func didMove(to view: SKView) {
         
@@ -42,11 +45,67 @@ class GameScene: SKScene {
         knob.position = CGPoint(x: 0, y: 0)
         addChild(disk)
         
-        player = SKSpriteNode(imageNamed: "up")
-        player = SKSpriteNode(imageNamed: "character")
-        player.size = CGSize(width: 130.0, height: 105.0)
+        let texture = SKTexture(imageNamed: "up")
+        player  = SKSpriteNode(texture: texture)
         player.position = CGPoint(x: screenWidth/2, y: 350)
         addChild(player)
+        
+        //hidden members position set up
+        spawnHiddenMembers()
+        
+        //label generator
+        generateMembersCollectedLabel()
+    }
+    
+    func spawnHiddenMembers() {
+        for i in 0...2 {
+            hiddenMembers.append(SKSpriteNode(imageNamed: "member\(i)"))
+            hiddenMembers[i].anchorPoint = CGPointZero
+            hiddenMembers[i].name = "member"
+            hiddenMembers[i].setScale(0.5)
+            if i == 0 {
+                hiddenMembers[i].position = CGPoint(x: -100, y: 0)
+            } else if i == 1 {
+                hiddenMembers[i].position = CGPoint(x: 100, y: -200)
+            } else {
+                hiddenMembers[i].position = CGPoint(x: 300, y: 200)
+            }
+            
+            addChild(hiddenMembers[i])
+        }
+    }
+    
+    func generateMembersCollectedLabel() {
+        membersCollectedLabel.text = "Members Collected: 0"
+        membersCollectedLabel.fontColor = SKColor.black
+        membersCollectedLabel.fontSize = 20
+        membersCollectedLabel.zPosition = 999
+        membersCollectedLabel.horizontalAlignmentMode = .left
+        membersCollectedLabel.verticalAlignmentMode = .bottom
+        membersCollectedLabel.position = CGPoint(x: 0, y: 0)
+        
+        addChild(membersCollectedLabel)
+    }
+    
+    func checkCollisions() {
+        var membersCollected: [SKSpriteNode] = []
+        enumerateChildNodes(withName: "member") { node, _ in
+            let member = node as! SKSpriteNode
+            if CGRectIntersectsRect(member.frame, self.player.frame) {
+                membersCollected.append(member)
+            }
+        }
+        
+        for member in membersCollected {
+            print("a member found")
+            collectHiddenMember(member: member)
+        }
+    }
+    
+    func collectHiddenMember(member: SKSpriteNode) {
+        member.removeFromParent()
+        numberOfMembersCollected += 1
+        membersCollectedLabel.text = "Members Collected: \(numberOfMembersCollected)"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -94,9 +153,62 @@ class GameScene: SKScene {
 //                print("bbb", tangent2)
 //                print()
                 
-//                switch {
-//                    case
-//                }
+                switch tangent2 {
+                 // >>> UP
+                case (Double.pi / 4) ..< (3 * Double.pi / 4):
+                     let texture = SKTexture(imageNamed: "up")
+                     player.texture = texture
+                     print("up")
+
+
+                 // >>> LEFT
+                 case (3 * Double.pi / 4) ... Double.pi:
+                     print("left-1")
+                     let texture = SKTexture(imageNamed: "left")
+                     player.texture = texture
+                 case (-1 * Double.pi) ... (-3 * Double.pi / 4):
+                     print("left-2")
+                     let texture = SKTexture(imageNamed: "left")
+                     player.texture = texture
+
+                 // >>> DOWN
+                 case (-3 * Double.pi / 4) ... (-1 * Double.pi / 4):
+                     print("down")
+                     let texture = SKTexture(imageNamed: "down")
+                     player.texture = texture
+
+                 // >>> RIGHT
+                 case (-1 * Double.pi / 4) ... 0 :
+                     print("right-2")
+                     let texture = SKTexture(imageNamed: "right")
+                     player.texture = texture
+                 case 0 ..< (Double.pi / 4):
+                     print("right-1")
+                     let texture = SKTexture(imageNamed: "right")
+                     player.texture = texture
+
+                 default:
+                     print("mantap")
+                 }
+
+
+ //                switch tangent2 {
+ //                case (Double.pi / 4) ... (3 * Double.pi / 4):
+ //                    player = SKSpriteNode(imageNamed: "up")
+ //                    print("up")
+ //                case (-3 * Double.pi / 4) ... (3 * Double.pi / 4):
+ //                    player = SKSpriteNode(imageNamed: "left")
+ //                    print("left")
+ //                case (-3 * Double.pi / 4) ... (-1 * Double.pi / 4):
+ //                    player = SKSpriteNode(imageNamed: "right")
+ //                    print("right")
+ //                case (-1  * Double.pi / 4) ... (Double.pi / 4):
+ //                    player = SKSpriteNode(imageNamed: "down")
+ //                    print("down")
+ //                default:
+ //                    player = SKSpriteNode(imageNamed: "up")
+ //                    print("mantap")
+ //                }
             }
         }
     }
@@ -108,11 +220,16 @@ class GameScene: SKScene {
         disk.run(moveDisk)
         self.isPressed = false
         disk.alpha = 0.3
-        
     }
     
     override func update(_ currentTime: TimeInterval) {
         camera?.position.x = player.position.x
+        membersCollectedLabel.position.x = (camera?.position.x)!
         camera?.position.y = player.position.y
+        membersCollectedLabel.position.y = (camera?.position.y)!
+    }
+    
+    override func didEvaluateActions() {
+        checkCollisions()
     }
 }
