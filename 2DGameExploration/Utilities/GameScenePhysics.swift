6fbdac1a08380ef94e858ce8e4991032c1eb87e9
daniel-bss <daniel.simamora2000@gmail.com>
2023-06-23 18:30:53 +0700

    add bitmask

---
+++ 2DGameExploration/Utilities/GameScenePhysics.swift
@@ -0,0 +1,33 @@
+//
+//  GameScenePhysics.swift
+//  2DGameExploration
+//
+//  Created by Daniel Bernard Sahala Simamora on 23/06/23.
+//
+
+import Foundation
+import SpriteKit
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+            print("aaa")
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+            print("bbb")
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.player != 0) && (secondBody.categoryBitMask & PhysicsCategory.portalA != 0)) {
+            if let player = firstBody.node as? SKSpriteNode,
+               let portal = secondBody.node as? SKSpriteNode {
+                print("\nMantap")
+            }
+        }
+        
+    }
+}
